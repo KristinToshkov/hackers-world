a9d9ff202ad42c4d80101aa255cfdd2d02305e44
JustKris <kristin10v@abv.bg>
2025-03-29 20:40:59 +0200

    Update ChatMessageClient.java

--- src/main/java/app/chatMessage/client/ChatMessageClient.java
+++ src/main/java/app/chatMessage/client/ChatMessageClient.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.UUID;
-
-@FeignClient(name = "chat-service", url = "http://localhost:8081/api/chat")
+// url changed to deployed microservice - previously was https://localhost:8081/api/chat
+@FeignClient(name = "chat-service", url = "https://chat-microservice-f0be7c121a26.herokuapp.com/api/chat")
 public interface ChatMessageClient {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
